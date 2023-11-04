9ec6e2d8a1eb815d5670047e71a02a5576c87925
shubham sharma <2022mt93103@wilp.bits-pilani.ac.in>
2023-11-04 16:22:26 +0530

    updated data.sql for reviews

--- Reviews-Service/src/main/resources/data.sql
+++ Reviews-Service/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO review(`id`, `content`, `username`, `rating`, `course_id`) values (1, 'Very Thorough study material', 'shubham', 4, 1);
-INSERT INTO review(`id`, `content`, `username`, `rating`, `course_id`) values (2, 'Learning backend development is very easy with this course', 'Sharma', 5, 2);
+INSERT INTO review(`id`, `content`, `username`, `rating`, `course_id`) values (111, 'Very Thorough study material', 'shubham', 4, 101);
+INSERT INTO review(`id`, `content`, `username`, `rating`, `course_id`) values (222, 'Learning backend development is very easy with this course', 'Sharma', 5, 102);
 
 
-INSERT INTO comment(`id`, `content`, `username`, `review_id`) values (1, 'Thanks for the comment', 'testingUser', 1);
-INSERT INTO comment(`id`, `content`, `username`, `review_id`) values (2, 'Is live classes also part of course?', 'student1', 1);
-INSERT INTO comment(`id`, `content`, `username`, `review_id`) values (3, 'Is the content updated?', 'student2', 2);
+INSERT INTO comment(`id`, `content`, `username`, `review_id`) values (167, 'Thanks for the comment', 'testingUser', 111);
+INSERT INTO comment(`id`, `content`, `username`, `review_id`) values (278, 'Is live classes also part of course?', 'student1', 111);
+INSERT INTO comment(`id`, `content`, `username`, `review_id`) values (389, 'Is the content updated?', 'student2', 222);
